6206e7188fa426fa0f524b11abba3322eab80439
Nicolas Barrera <dev+DrummerHead@cuboxsa.com>
2011-08-11 21:18:59 -0300

    positioning of the player on the middle for all cell sizes

--- dc.js
+++ dc.js
@@ -1,21 +1,18 @@
 // initial variables
-var wW = $(window).width(),
-    wH = $(window).height(),
+var wW,
+    wH,
     d,
     celld,
     size = 8, //according to taste
     cellsNum = size*size,
     cells,
     uM = 0, //margin-top in case portrait
-    li = '<li></li>',
+    li = '',
     appendHtml = '',
-    rT; //rT is resize timeout
-
+    rT, //rT is resize timeout
+    firstPos;
 
 // function to set a the number to which all sizes are calculated according to landscape vs portrait length; using this, sets size for cells
-
-// console.log('wW = '+wW+', wH = '+wH)
-
 function sizeCells() {
   wW = $(window).width();
   wH = $(window).height();
@@ -29,22 +26,26 @@ function sizeCells() {
   celld = d/size;
   //sets the size of the container and cells
   $('li').css({width:celld,height:celld});
-  $('#w').css({width:d,marginTop:uM}); //+1 to avoid float falling - +1 was removed, only necessary for odd size
+  $('#w').css({width:d+1,marginTop:uM}); //+1 to avoid float falling - +1 was removed, only necessary for odd size
 }
 
 // appends cells according to size variable and does first draw
-
 for (var i = 0; i < cellsNum; i++) {
-  appendHtml += li;
+  appendHtml += "<li class='c"+(i+1)+"'></li>";
 }
 $('#w').append(appendHtml)
 sizeCells()
 
 // cell redrawing/resizing on windows resize
-
 $(window).resize(function(){
   clearTimeout(rT);
   rT = setTimeout(function(){
     sizeCells()
   }, 100)
 });
+
+// Drawing of the character
+// firstPos = (cellsNum/2)+((size/2)+1);
+firstPos = ((Math.round(size/2))*size)-(Math.floor(size/2))
+$('.c'+firstPos).attr('id','p');
+
